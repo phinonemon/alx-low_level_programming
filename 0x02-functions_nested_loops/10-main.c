a0237ff1c61216e09b53f6a873c1572f40781f81
phinonemon <philemonesselmoses@gmail.com>
2023-06-21 04:17:42 +0000

    Task 0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int add(int a, int b);
+
+int main(void)
+{
+    int n;
+
+    n = add(89, 9);
+    printf("%d\n", n);
+    return 0;
+}
---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,12 @@
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void jack_bauer(void);
+
+int main(void)
+{
+    jack_bauer();
+    return 0;
+}
